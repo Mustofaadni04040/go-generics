b56181cd0ef53fd9393389e86433e478482f8149
mustofa adny <adniadni04@gmail.com>
2025-06-09 13:56:39 +0700

    multiple parameter generics

--- type_parameter_test.go
+++ type_parameter_test.go
@@ -25,3 +25,13 @@ func TestLength(t *testing.T) {
 func TestSample(t *testing.T) {
 	assert.True(t, true)
 }
+
+func MultipeParameter[T any, X any](param1 T, param2 X) {
+	fmt.Println(param1)
+	fmt.Println(param2)
+}
+
+func TestMutipleParameter(t *testing.T) {
+	MultipeParameter[string, int]("Belajar Golang Generics", 10)
+	MultipeParameter[int, string](100, "Belajar Golang Generics")
+}
